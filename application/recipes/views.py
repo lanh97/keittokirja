1d191c0b6532387603f63a8cea1fb4c353a00fbc
lanh97 <lam.thinh1997@yahoo.co>
2019-04-04 14:35:38 +0300

    pieni lisäys äskeiseen

--- application/recipes/views.py
+++ application/recipes/views.py
@@ -4,20 +4,23 @@ from flask_login import login_required
 from application.recipes.forms import RecipeForm
 from application.recipes.models import Recipe
 
+
 @app.route("/recipes/new/")
-def ingredients_form():
+def recipes_form():
     return render_template("recipes/new.html", form=RecipeForm())
+
 
 @app.route("/reicipes/", methods=["POST"])
 @login_required
-def ingredients_create():
+def recipes_create():
     form = RecipeForm(request.form)
 
     if not form.validate():
         return render_template("recipes/new.html", form=form)
 
-    recip = Recipe(form.name.data, form.lisatiedot.data)
+    recip = Recipe(form.name.data, form.lisatiedot.data,
+                   form.cookinginstruction.data)
     db.session().add(recip)
     db.session().commit()
 
-    return redirect(url_for("ingredients_index"))
+    return redirect(url_for("recipes_form"))

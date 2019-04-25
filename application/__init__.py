b07cab6e2ea9f8fa3a750df7dfa2cea44ca31343
lanh97 <lam.thinh1997@yahoo.co>
2019-04-25 15:46:43 +0300

    authorisointi ominaisuus

--- application/__init__.py
+++ application/__init__.py
@@ -1,52 +1,77 @@
+from functools import wraps
+from flask_login import LoginManager
+from application.auth.models import User
+from application.recipesingredient import views
+from application.recipesingredient import models
+from application.auth import views
+from application.auth import models
+from application.recipes import views
+from application.recipes import models
+from application.ingredients import views
+from application.ingredients import models
+from application import views
+from flask_sqlalchemy import SQLAlchemy
+import os
 from flask import Flask
 from os import urandom
 app = Flask(__name__)
 
-import os
-
-from flask_sqlalchemy import SQLAlchemy
 
 if os.environ.get("HEROKU"):
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 else:
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///keittokirja.db"    
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///keittokirja.db"
     app.config["SQLALCHEMY_ECHO"] = True
 
-  
+
 db = SQLAlchemy(app)
-
-from application import views
-
-from application.ingredients import models
-from application.ingredients import views
-
-from application.recipes import models
-from application.recipes import views
-
-from application.auth import models
-from application.auth import views
-
-from application.recipesingredient import models
-from application.recipesingredient import views
-
-from application.auth.models import User
-from os import urandom
 
 
 app.config["SECRET_KEY"] = urandom(32)
 
-from flask_login import LoginManager
 login_manager = LoginManager()
 login_manager.init_app(app)
 
 login_manager.login_view = "auth_login"
 login_manager.login_message = "Please login to use this functionality."
 
+# roles in login_required
+
+
+def login_required(role="ANY"):
+    def wrapper(fn):
+        @wraps(fn)
+        def decorated_view(*args, **kwargs):
+            if not current_user:
+                return login_manager.unauthorized()
+
+            if not current_user.is_authenticated():
+                return login_manager.unauthorized()
+
+            unauthorized = False
+
+            if role != "ANY":
+                unauthorized = True
+
+                for user_role in current_user.roles():
+                    if user_role == role:
+                        unauthorized = False
+                        break
+
+            if unauthorized:
+                return login_manager.unauthorized()
+
+            return fn(*args, **kwargs)
+        return decorated_view
+    return wrapper
+
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(user_id)
 
-try: 
+
+try:
     db.create_all()
 except:
     pass
--- application/auth/models.py
+++ application/auth/models.py
@@ -7,7 +7,7 @@ class User(Base):
     __tablename__ = "account"
 
     name = db.Column(db.String(144), nullable=False)
-    username = db.Column(db.String(144), nullable=False, unique = True)
+    username = db.Column(db.String(144), nullable=False, unique=True)
     password = db.Column(db.String(144), nullable=False)
 
     def __init__(self, name, username, password):
@@ -26,3 +26,6 @@ class User(Base):
 
     def is_authenticated(self):
         return True
+
+    def roles(self):
+        return ["ADMIN"]
